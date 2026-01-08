e73f6e0004c753b6da34fe8aa44341da8d83f724
Diariatou <diarymbengue98@gmail.com>
2026-01-08 19:54:16 +0100

    Fix Render deployment

--- config/settings.py
+++ config/settings.py
@@ -30,13 +30,9 @@ MEDIA_ROOT = BASE_DIR / 'media'
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG") == "True"
+DEBUG = True
 
-ALLOWED_HOSTS = [
-    'sunuremode.onrender.com',
-    'localhost',
-    '127.0.0.1',
-]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
