ad54f7ce138db9921699feab993e794a796e1dc2
Diariatou <diarymbengue98@gmail.com>
2026-01-11 21:06:38 +0100

    Fix settings for Render (DEBUG, DB, allowed hosts)

--- config/settings.py
+++ config/settings.py
@@ -33,7 +33,11 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = [
+    "sunuremode.onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
