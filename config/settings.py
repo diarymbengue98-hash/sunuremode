df1d115bc60d59f46042121b21f31bb8fc1f45dc
Diariatou <diarymbengue98@gmail.com>
2026-05-13 18:21:19 +0200

    fix postgres ssl

--- config/settings.py
+++ config/settings.py
@@ -63,9 +63,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 
 
+
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL")
+    'default': dj_database_url.parse(
+        os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 # 🔑 PASSWORD
